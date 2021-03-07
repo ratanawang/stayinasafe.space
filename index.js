e9166ec16c626c4b691868839454a729075ced73
Nanxin Qin <nanxinqin@hotmail.com>
2021-03-07 08:28:48 -0800

    js

--- index.js
+++ index.js
@@ -23,68 +23,62 @@ function getReportLocation() {
 
 function initMap(){
     const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 3,
+        zoom: ,
+        // center: {lat: 49.2827, lng: -123.1207}
         center: { lat: -25.344, lng: 131.036 }
     });
-    const VerbalImage =
-    "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" ;
-    const VerbalMarkers = VerbalLocations.map((location, i) => {
-        return new google.maps.Marker({
-            position: location,
-            icon: VerbalImage,
-            map: map,
-        });
-    });
+    const data = {
+      'type': 'verbal',
+      'lat': -33.827293,
+      'lng': 150.202919,
+      'time': '02/19/2021 11:33',
+      'context': 'goddd'
+    };
 
-    const PhysicalImage =
-    "http://maps.google.com/mapfiles/ms/icons/red-dot.png" ;
-    const PhysicalMarkers = PhysicalLocations.map((location, i) => {
-        return new google.maps.Marker({
-            position: location,
-            icon: PhysicalImage,
-            map: map,
-        });
-    });
-
-    const StalkImage =
-    "http://maps.google.com/mapfiles/ms/icons/green-dot.png" ;
-    const StalkMarkers = StalkLocations.map((location, i) => {
-        return new google.maps.Marker({
-            position: location,
-            icon: StalkImage,
-            map: map,
-        });
-    });
-
-}
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+    fetch('http://localhost:5000', options);
 
 
-const VerbalLocations = [
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-];
-const PhysicalLocations = [
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-];
-const StalkLocations = [
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
-];
+    fetch("http://localhost:5000", { referrerPolicy: 'no-referrer-when-downgrade' })
+      .then(resp => resp.json())
+      .then(resp=> {
+          const StalkLocations = resp.stalkLocations;
+          const StalkImage =
+          "http://maps.google.com/mapfiles/ms/icons/green-dot.png" ;
+          const StalkMarkers = StalkLocations.map((location, i) => {
+              return new google.maps.Marker({
+                  position: location,
+                  icon: StalkImage,
+                  map: map,
+              });
+          });
+
+          const PhysicalLocations = resp.physicalLocations;
+          const PhysicalImage =
+          "http://maps.google.com/mapfiles/ms/icons/red-dot.png" ;
+          const PhysicalMarkers = PhysicalLocations.map((location, i) => {
+              return new google.maps.Marker({
+                  position: location,
+                  icon: PhysicalImage,
+                  map: map,
+              });
+          });
+
+          const VerbalLocations = resp.verbalLocations;
+          const VerbalImage =
+          "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" ;
+          const VerbalMarkers = VerbalLocations.map((location, i) => {
+              return new google.maps.Marker({
+                  position: location,
+                  icon: VerbalImage,
+                  map: map,
+              });
+          });
+        })
+
